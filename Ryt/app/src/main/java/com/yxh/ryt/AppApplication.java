45a0a185f351bd9a5c33f092db6534adf2d644a2
wuhongjie <wuhongjie-1234@163.com>
2016-03-30 19:55:52 +0800

    ddddddhh

--- Ryt/app/src/main/java/com/yxh/ryt/AppApplication.java
+++ Ryt/app/src/main/java/com/yxh/ryt/AppApplication.java
@@ -11,7 +11,6 @@ public class AppApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		context=this;
-		Log.d("dddddddddd","fffffffffggghhddddd");
 	}
 
 	//获取全局Context
