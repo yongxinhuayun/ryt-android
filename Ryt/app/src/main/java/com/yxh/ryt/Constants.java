92f9cba3629a478f893e132a70c47f67d4cc7e0c
wuhongjie <wuhongjie-1234@163.com>
2016-04-01 11:16:09 +0800

    New changelist

--- Ryt/app/src/main/java/com/yxh/ryt/Constants.java
+++ Ryt/app/src/main/java/com/yxh/ryt/Constants.java
@@ -21,9 +21,4 @@ public class Constants {
     public static final String WX_PAY_ACTION_FAILURE = "wx_pay_failure";
     public static final String WX_INSTALL_ACTION = "wx_install_action";
     public static final String WX_NOTINSTALL_ACTION = "wx_notinstall_action";
-
-
-
-
-
 }
