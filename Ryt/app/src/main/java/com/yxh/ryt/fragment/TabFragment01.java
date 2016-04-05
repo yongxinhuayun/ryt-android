fd511a868908a2a5547c209cbde52e7a59c5d290
wuhongjie <wuhongjie-1234@163.com>
2016-04-05 18:53:26 +0800

    New changelist

--- Ryt/app/src/main/java/com/yxh/ryt/fragment/TabFragment01.java
+++ Ryt/app/src/main/java/com/yxh/ryt/fragment/TabFragment01.java
@@ -15,21 +15,29 @@ import com.yxh.ryt.Constants;
 import com.yxh.ryt.R;
 import com.yxh.ryt.adapter.TabPageIndicatorAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016-4-4.
  */
 public class TabFragment01 extends  BaseFragment {
+    List<BaseFragment> fragments=new ArrayList<>();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fragments.add(new RongZiItemFragment());
+        fragments.add(new RongZiItemFragment());
+        fragments.add(new RongZiItemFragment());
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_01, null);
-        FragmentPagerAdapter adapter = new TabPageIndicatorAdapter(getActivity().getSupportFragmentManager());
+        FragmentPagerAdapter adapter = new TabPageIndicatorAdapter(getActivity().getSupportFragmentManager(),fragments);
         ViewPager pager = (ViewPager)view.findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
 
         //实例化TabPageIndicator然后设置ViewPager与之关联
@@ -55,5 +63,14 @@ public class TabFragment01 extends  BaseFragment {
             }
         });
         return view;
+    }
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    private void  loadData(){
+
     }
 }
