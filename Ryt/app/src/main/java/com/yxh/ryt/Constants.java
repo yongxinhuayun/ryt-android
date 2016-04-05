493770f3648ead54ef8d888741137f01ceb248e8
iamdurui <durui911225_>
2016-04-05 14:39:03 +0800

    8888888888888888888

--- Ryt/app/src/main/java/com/yxh/ryt/Constants.java
+++ Ryt/app/src/main/java/com/yxh/ryt/Constants.java
@@ -3,8 +3,8 @@ package com.yxh.ryt;
 public class Constants {
 
     //网络请求基地址
-//    public static final String BASE_PATH="http://192.168.1.69:8001/app/";
-    public static final String BASE_PATH="http://j.efeiyi.com:8080/app-wikiServer/app/";
+    public static final String BASE_PATH="http://192.168.1.69:8001/app/";
+    //public static final String BASE_PATH="http://j.efeiyi.com:8080/app-wikiServer/app/";
     //融艺投APP_KEY
     public static final String APP_KEY="BL2QEuXUXNoGbNeHObD4EzlX+KuGc70U";
     //微信APP_ID
