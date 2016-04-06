8a5b23a89139251f8b2044633a1a3f2a085153e6
iamdurui <durui911225_>
2016-04-06 21:17:45 +0800

    nnnnnnnnnnn

--- Ryt/app/src/main/java/com/yxh/ryt/activity/BaseActivity.java
+++ Ryt/app/src/main/java/com/yxh/ryt/activity/BaseActivity.java
@@ -7,6 +7,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
+import com.yxh.ryt.R;
+
+import cn.jpush.android.api.CustomPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
+
 public abstract class BaseActivity extends FragmentActivity {
 
     protected InputMethodManager inputMethodManager;
@@ -18,17 +23,20 @@ public abstract class BaseActivity extends FragmentActivity {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        CustomPushNotificationBuilder builder = new CustomPushNotificationBuilder(this, R.layout.notification_layout, R.id.noti_image, R.id.noti_title, R.id.noti_text);
+        JPushInterface.setPushNotificationBuilder(1, builder);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        JPushInterface.onResume(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        JPushInterface.onPause(this);
     }
 
     @Override
