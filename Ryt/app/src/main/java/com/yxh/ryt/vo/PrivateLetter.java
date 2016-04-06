2ba94c926fecce09afa76734bbbb361dd202fd2b
iamdurui <durui911225_>
2016-04-06 19:45:14 +0800

    nnnnnnnnnnn

--- Ryt/app/src/main/java/com/yxh/ryt/vo/PrivateLetter.java
+++ Ryt/app/src/main/java/com/yxh/ryt/vo/PrivateLetter.java
@@ -4,47 +4,37 @@ package com.yxh.ryt.vo;
  * Created by Administrator on 2016/4/5.
  */
 public class PrivateLetter {
-    private String imageUrl;
-    private String context;
-    private String createDatetime;
-    private String isWatch;
+    private String content;
+    private long createDatetime;
+    private int isWatch;
 
-    public PrivateLetter(String isWatch, String createDatetime, String context, String imageUrl) {
+    public PrivateLetter(String content, int isWatch, long createDatetime) {
+        this.content = content;
         this.isWatch = isWatch;
         this.createDatetime = createDatetime;
-        this.context = context;
-        this.imageUrl = imageUrl;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public String getContent() {
+        return content;
     }
 
-    public String getIsWatch() {
-        return isWatch;
-    }
-
-    public String getCreateDatetime() {
+    public long getCreateDatetime() {
         return createDatetime;
     }
 
-    public String getContext() {
-        return context;
+    public int getIsWatch() {
+        return isWatch;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setContent(String content) {
+        this.content = content;
     }
 
-    public void setIsWatch(String isWatch) {
-        this.isWatch = isWatch;
-    }
-
-    public void setCreateDatetime(String createDatetime) {
+    public void setCreateDatetime(long createDatetime) {
         this.createDatetime = createDatetime;
     }
 
-    public void setContext(String context) {
-        this.context = context;
+    public void setIsWatch(int isWatch) {
+        this.isWatch = isWatch;
     }
 }
