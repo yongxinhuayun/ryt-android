271875f7689c899e9e3c5d0bc5a63c6675b39ee1
iamdurui <durui911225_>
2016-04-07 18:47:43 +0800

    nnnnnnnnnnn

--- Ryt/app/src/main/java/com/yxh/ryt/vo/Notification.java
+++ Ryt/app/src/main/java/com/yxh/ryt/vo/Notification.java
@@ -7,15 +7,21 @@ public class Notification {
     private String content;
     private long createDatetime;
     private int isWatch;
+    private Notifaction_user notifaction_user;
 
-    public Notification(String content, int isWatch, long createDatetime) {
+    public Notification(String content, Notifaction_user notifaction_user, int isWatch, long createDatetime) {
         this.content = content;
+        this.notifaction_user = notifaction_user;
         this.isWatch = isWatch;
         this.createDatetime = createDatetime;
     }
 
     public String getContent() {
         return content;
+    }
+
+    public Notifaction_user getNotifaction_user() {
+        return notifaction_user;
     }
 
     public int getIsWatch() {
@@ -28,6 +34,10 @@ public class Notification {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void setNotifaction_user(Notifaction_user notifaction_user) {
+        this.notifaction_user = notifaction_user;
     }
 
     public void setIsWatch(int isWatch) {
