053709a8de8a8178c48dca979b938f49669e9336
iamdurui <durui911225_>
2016-04-07 20:49:20 +0800

    nnnnnnnnnnn

--- Ryt/app/src/main/java/com/yxh/ryt/vo/Notification.java
+++ Ryt/app/src/main/java/com/yxh/ryt/vo/Notification.java
@@ -7,44 +7,44 @@ public class Notification {
     private String content;
     private long createDatetime;
     private int isWatch;
-    private Notifaction_user notifaction_user;
+    private Notifaction_user fromUser;
 
-    public Notification(String content, Notifaction_user notifaction_user, int isWatch, long createDatetime) {
-        this.content = content;
-        this.notifaction_user = notifaction_user;
-        this.isWatch = isWatch;
+    public Notification(long createDatetime, String content, int isWatch, Notifaction_user fromUser) {
         this.createDatetime = createDatetime;
+        this.content = content;
+        this.isWatch = isWatch;
+        this.fromUser = fromUser;
     }
 
     public String getContent() {
         return content;
     }
 
-    public Notifaction_user getNotifaction_user() {
-        return notifaction_user;
+    public long getCreateDatetime() {
+        return createDatetime;
     }
 
     public int getIsWatch() {
         return isWatch;
     }
 
-    public long getCreateDatetime() {
-        return createDatetime;
+    public Notifaction_user getFromUser() {
+        return fromUser;
     }
 
     public void setContent(String content) {
         this.content = content;
     }
 
-    public void setNotifaction_user(Notifaction_user notifaction_user) {
-        this.notifaction_user = notifaction_user;
+    public void setCreateDatetime(long createDatetime) {
+        this.createDatetime = createDatetime;
     }
 
     public void setIsWatch(int isWatch) {
         this.isWatch = isWatch;
     }
 
-    public void setCreateDatetime(long createDatetime) {
-        this.createDatetime = createDatetime;
+    public void setFromUser(Notifaction_user fromUser) {
+        this.fromUser = fromUser;
     }
 }
