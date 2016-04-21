131feb59e0ebd17cb8d57c50e5460239eed33ed0
wuhongjie <wuhongjie-1234@163.com>
2016-04-21 16:23:19 +0800

    fcdfd不能

--- Ryt/app/src/main/java/com/yxh/ryt/util/NetRequestUtil.java
+++ Ryt/app/src/main/java/com/yxh/ryt/util/NetRequestUtil.java
@@ -16,41 +16,58 @@ import java.util.Map;
  */
 public class NetRequestUtil<T> {
 
-    public static <T> void post(String url,Map<String,String> t,Callback<T> callback){
+    public static <T> void post(final String url, final Map<String, String> t, final Callback<T> callback) {
         System.out.println(AppApplication.getSingleGson().toJson(t));
-        OkHttpUtils
-                .postString()
-                .url(url)
-                .content(AppApplication.getSingleGson().toJson(t))
-                .build()
-                .execute(callback);
+        new Thread() {
+            @Override
+            public void run() {
+                OkHttpUtils
+                        .postString()
+                        .url(url)
+                        .content(AppApplication.getSingleGson().toJson(t))
+                        .build()
+                        .execute(callback);
+            }
+        }.start();
+
     }
 
-    public static <T> void get(String url,Map<String,String> paramsMap,Callback<T> callback){
-        GetBuilder getBuilder = OkHttpUtils.get();
-        getBuilder.url(url);
-        Iterator<Map.Entry<String, String>> it = paramsMap.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, String> entry = it.next();
-            getBuilder.addParams(entry.getKey(),  entry.getValue());
-        }
-        RequestCall build = getBuilder.build();
-        build.execute(callback);
+    public static <T> void get(final String url, final Map<String, String> paramsMap, final Callback<T> callback) {
+        new Thread() {
+            @Override
+            public void run() {
+                GetBuilder getBuilder = OkHttpUtils.get();
+                getBuilder.url(url);
+                Iterator<Map.Entry<String, String>> it = paramsMap.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> entry = it.next();
+                    getBuilder.addParams(entry.getKey(), entry.getValue());
+                }
+                RequestCall build = getBuilder.build();
+                build.execute(callback);
+            }
+        }.start();
     }
-    public static <T> void postFile(String url,String fileKey,Map<String,File> fileMap,Map<String,String> paramsMap,  Map<String, String> headers,Callback<T> callback){
-        PostFormBuilder post = OkHttpUtils.post();
-        Iterator<Map.Entry<String, File>> it = fileMap.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, File> entry = it.next();
-            post.addFile(fileKey,entry.getKey(),  entry.getValue());
-        }
-        post.url(url);
-        post.params(paramsMap);
-        post.headers(headers);
-        RequestCall build = post.build();
-        build.connTimeOut(500000);
-        build.readTimeOut(500000);
-        build.writeTimeOut(500000);
-        build.execute(callback);
+
+    public static <T> void postFile(final String url, final String fileKey, final Map<String, File> fileMap, final Map<String, String> paramsMap, final Map<String, String> headers, final Callback<T> callback) {
+        new Thread() {
+            @Override
+            public void run() {
+                PostFormBuilder post = OkHttpUtils.post();
+                Iterator<Map.Entry<String, File>> it = fileMap.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, File> entry = it.next();
+                    post.addFile(fileKey, entry.getKey(), entry.getValue());
+                }
+                post.url(url);
+                post.params(paramsMap);
+                post.headers(headers);
+                RequestCall build = post.build();
+                build.connTimeOut(500000);
+                build.readTimeOut(500000);
+                build.writeTimeOut(500000);
+                build.execute(callback);
+            }
+        }.start();
     }
 }
