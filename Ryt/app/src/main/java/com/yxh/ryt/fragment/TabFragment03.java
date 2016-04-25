b6f93c21ce0ab2ef0618e69f1018d6a96b14e65a
iamdurui <durui911225_>
2016-04-25 19:00:47 +0800

    66666666666666666

--- Ryt/app/src/main/java/com/yxh/ryt/fragment/TabFragment03.java
+++ Ryt/app/src/main/java/com/yxh/ryt/fragment/TabFragment03.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import com.yxh.ryt.AppApplication;
 import com.yxh.ryt.Constants;
 import com.yxh.ryt.R;
+import com.yxh.ryt.activity.BaseActivity;
 import com.yxh.ryt.activity.CommentActivity;
 import com.yxh.ryt.activity.NotificationActivity;
 import com.yxh.ryt.activity.PrivateLetterActivity;
@@ -42,23 +44,51 @@ public class TabFragment03 extends  BaseFragment {
     CircleImageView circleComment;
     @Bind(R.id.ft3_ci_privateLetter)
     CircleImageView circlePrivateLetter;
-    private BadgeView bv;
+    private BadgeView bvNotification,bvComment,bvPrivateLetter;
+    int count=0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_03, null);
         ButterKnife.bind(this, view);
+        loadData();
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        loadData();
     }
 
     @Override
     protected void lazyLoad() {
 
+    }
+    public void updateWatchedStatus(String group, final Class cls){
+        Map<String,String> paramsMap=new HashMap<>();
+        paramsMap.put("userId", "ieatht97wfw30hfd");
+        paramsMap.put("group", group);
+        paramsMap.put("timestamp", System.currentTimeMillis() + "");
+        try {
+            AppApplication.signmsg= EncryptUtil.encrypt(paramsMap);
+            paramsMap.put("signmsg", AppApplication.signmsg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        NetRequestUtil.post(Constants.BASE_PATH + "updateWatchedStatus.do", paramsMap, new LoginCallBack() {
+            @Override
+            public void onError(Call call, Exception e) {
+                System.out.println("失败了");
+            }
+
+            @Override
+            public void onResponse(Map<String, Object> response) {
+                if (response.get("resultCode").equals("0")){
+                    Log.d("resultCoderesultCoderesultCode","reresultCoderesultCoderesultCode");
+                    Intent intent=new Intent(getActivity(), cls);
+                    getActivity().startActivity(intent);
+                }
+            }
+        });
     }
     private void loadData() {
         Map<String,String> paramsMap=new HashMap<>();
@@ -78,61 +108,56 @@ public class TabFragment03 extends  BaseFragment {
 
             @Override
             public void onResponse(Map<String, Object> response) {
-
+                bvPrivateLetter = new BadgeView(getActivity(),circlePrivateLetter);
+                bvNotification = new BadgeView(getActivity(),circleNotification);
+                bvComment = new BadgeView(getActivity(),circleComment);
                 if (response.get("resultCode").equals("0")){
                     if ( ((Double) response.get("noticeNum")) !=0){
-                        bv = new BadgeView(getActivity(),circleNotification);
-                        bv.setText(response.get("noticeNum")+"");
-                        bv.setTextColor(Color.WHITE);
-                        bv.setTextSize(7);
-                        bv.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
-                        bv.show();
+                        bvNotification.setText(response.get("noticeNum")+"");
+                        bvNotification.setTextColor(Color.WHITE);
+                        bvNotification.setTextSize(7);
+                        bvNotification.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
+                        bvNotification.show();
                     }else {
-                        bv = new BadgeView(getActivity(),circleNotification);
-                        bv.setVisibility(View.GONE);
+                        bvNotification.setVisibility(View.GONE);
                     }
                     if (((Double) response.get("commentNum")) !=0){
-                        bv = new BadgeView(getActivity(),circleComment);
-                        bv.setText(response.get("commentNum")+"");
-                        bv.setTextColor(Color.WHITE);
-                        bv.setTextSize(7);
-                        bv.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
-                        bv.show();
+                        bvComment = new BadgeView(getActivity(),circleComment);
+                        bvComment.setText(response.get("commentNum")+"");
+                        bvComment.setTextColor(Color.WHITE);
+                        bvComment.setTextSize(7);
+                        bvComment.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
+                        bvComment.show();
                     }else {
-                        bv = new BadgeView(getActivity(),circleComment);
-                        bv.setVisibility(View.GONE);
+                        bvComment.setVisibility(View.GONE);
                     }
                     if (((Double) response.get("messageNum")) !=0){
-                        bv = new BadgeView(getActivity(),circlePrivateLetter);
-                        bv.setText(response.get("messageNum")+"");
-                        bv.setTextColor(Color.WHITE);
-                        bv.setTextSize(7);
-                        bv.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
-                        bv.show();
+                        bvPrivateLetter.setText(response.get("messageNum")+"");
+                        bvPrivateLetter.setTextColor(Color.WHITE);
+                        bvPrivateLetter.setTextSize(7);
+                        bvPrivateLetter.setBadgePosition(BadgeView.POSITION_TOP_RIGHT); //默认
+                        bvPrivateLetter.show();
                     }else {
-                        bv = new BadgeView(getActivity(),circlePrivateLetter);
-                        bv.setVisibility(View.GONE);
+                        bvPrivateLetter.setVisibility(View.GONE);
                     }
                 }
             }
         });
     }
-
     @OnClick({ R.id.ft3_ll_notification, R.id.ft3_ll_comment, R.id.ft3_ll_privateLetter,R.id.tab03_ib_back })
     public void onClick(View view){
-        Intent intent=null;
         switch (view.getId()){
             case R.id.ft3_ll_notification:
-                intent=new Intent(getActivity(), NotificationActivity.class);
-                getActivity().startActivity(intent);
+                updateWatchedStatus("notification",NotificationActivity.class);
+                bvNotification.setVisibility(View.GONE);
                 break;
             case R.id.ft3_ll_comment:
-                intent=new Intent(getActivity(), CommentActivity.class);
-                getActivity().startActivity(intent);
+                updateWatchedStatus("comment", CommentActivity.class);
+                bvComment.setVisibility(View.GONE);
                 break;
             case R.id.ft3_ll_privateLetter:
-                intent=new Intent(getActivity(), PrivateLetterActivity.class);
-                getActivity().startActivity(intent);
+                updateWatchedStatus("message", PrivateLetterActivity.class);
+                bvPrivateLetter.setVisibility(View.GONE);
                 break;
             case R.id.tab03_ib_back:
                 getActivity().finish();
