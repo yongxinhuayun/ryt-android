01c64d6cd46d98838526c50534a574c3f64cfb88
iamdurui <durui911225_>
2016-04-25 16:58:34 +0800

    66666666666666666

---
+++ Ryt/app/src/main/java/com/yxh/ryt/fragment/AttentionItemFragment.java
@@ -0,0 +1,138 @@
+package com.yxh.ryt.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.google.gson.reflect.TypeToken;
+import com.yxh.ryt.AppApplication;
+import com.yxh.ryt.Constants;
+import com.yxh.ryt.R;
+import com.yxh.ryt.activity.RongZiXQActivity;
+import com.yxh.ryt.adapter.CommonAdapter;
+import com.yxh.ryt.adapter.ViewHolder;
+import com.yxh.ryt.callback.AttentionListCallBack;
+import com.yxh.ryt.callback.RongZiListCallBack;
+import com.yxh.ryt.custemview.AutoListView;
+import com.yxh.ryt.util.EncryptUtil;
+import com.yxh.ryt.util.NetRequestUtil;
+import com.yxh.ryt.util.Utils;
+import com.yxh.ryt.vo.ArtUserFollowed;
+import com.yxh.ryt.vo.RongZi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+
+
+public class AttentionItemFragment extends BaseFragment implements AutoListView.OnRefreshListener,
+		AutoListView.OnLoadListener {
+	private AutoListView lstv;
+	private CommonAdapter<ArtUserFollowed> attentionCommonAdapter;
+	private List<ArtUserFollowed> attentionDatas;
+	private int currentPage=1;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		attentionDatas=new ArrayList<ArtUserFollowed>();
+	}
+	private void LoadData(final int state,int pageNum) {
+		Map<String,String> paramsMap=new HashMap<>();
+		paramsMap.put("pageSize", Constants.pageSize + "");
+		paramsMap.put("pageNum", pageNum + "");
+		paramsMap.put("timestamp", System.currentTimeMillis() + "");
+		try {
+			AppApplication.signmsg= EncryptUtil.encrypt(paramsMap);
+			paramsMap.put("signmsg", AppApplication.signmsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		NetRequestUtil.post(Constants.BASE_PATH + "investorIndex.do", paramsMap, new AttentionListCallBack() {
+			@Override
+			public void onError(Call call, Exception e) {
+				e.printStackTrace();
+				System.out.println("失败了");
+			}
+
+			@Override
+			public void onResponse(Map<String, Object> response) {
+				if (state == AutoListView.REFRESH) {
+					lstv.onRefreshComplete();
+					attentionDatas.clear();
+					List<ArtUserFollowed> objectList = AppApplication.getSingleGson().fromJson(AppApplication.getSingleGson().toJson(response.get("objectList")), new TypeToken<List<ArtUserFollowed>>() {
+					}.getType());
+					if (null == objectList || objectList.size() == 0) {
+						lstv.setResultSize(0);
+					}
+					if (null != objectList && objectList.size() > 0) {
+						lstv.setResultSize(objectList.size());
+						attentionDatas.addAll(objectList);
+						attentionCommonAdapter.notifyDataSetChanged();
+					}
+					return;
+				}
+				if (state == AutoListView.LOAD) {
+					lstv.onLoadComplete();
+					List<ArtUserFollowed> objectList = AppApplication.getSingleGson().fromJson(AppApplication.getSingleGson().toJson(response.get("objectList")), new TypeToken<List<ArtUserFollowed>>() {
+					}.getType());
+					if (null == objectList || objectList.size() == 0) {
+						lstv.setResultSize(1);
+					}
+					if (null != objectList && objectList.size() > 0) {
+						lstv.setResultSize(objectList.size());
+						attentionDatas.addAll(objectList);
+						attentionCommonAdapter.notifyDataSetChanged();
+					}
+					return;
+				}
+			}
+		});
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View contextView = inflater.inflate(R.layout.fragment_item, container, false);
+		lstv = (AutoListView) contextView.findViewById(R.id.lstv);
+		lstv.setPageSize(Constants.pageSize);
+		attentionCommonAdapter=new CommonAdapter<ArtUserFollowed>(AppApplication.getSingleContext(),attentionDatas,R.layout.fragment_attention_item) {
+			@Override
+			public void convert(ViewHolder helper, ArtUserFollowed item) {
+
+			}
+		};
+		lstv.setAdapter(attentionCommonAdapter);
+		lstv.setOnRefreshListener(this);
+		lstv.setOnLoadListener(this);
+		return contextView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+	}
+	@Override
+	protected void lazyLoad() {
+		if(attentionDatas!=null&&attentionDatas.size()>0)return;
+		LoadData(AutoListView.REFRESH, currentPage);
+	}
+	@Override
+	public void onRefresh() {
+		currentPage=1;
+		LoadData(AutoListView.REFRESH,currentPage);
+	}
+
+	@Override
+	public void onLoad() {
+		currentPage++;
+		LoadData(AutoListView.LOAD, currentPage);
+	}
+
+}
