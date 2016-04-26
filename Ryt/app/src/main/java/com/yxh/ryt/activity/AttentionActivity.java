a26449fefb19e3ea329dbb3398893c41127cb506
iamdurui <durui911225_>
2016-04-26 18:23:45 +0800

    66666666666666666

--- Ryt/app/src/main/java/com/yxh/ryt/activity/AttentionActivity.java
+++ Ryt/app/src/main/java/com/yxh/ryt/activity/AttentionActivity.java
@@ -1,22 +1,15 @@
 package com.yxh.ryt.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.viewpagerindicator.TabPageIndicator;
 import com.yxh.ryt.R;
 import com.yxh.ryt.adapter.AttentionIndicatorAdapter;
-import com.yxh.ryt.adapter.IndexTabPageIndicatorAdapter;
-import com.yxh.ryt.fragment.AttentionItemFragment;
+import com.yxh.ryt.fragment.AttentionArtItemFragment;
+import com.yxh.ryt.fragment.AttentionUserItemFragment;
 import com.yxh.ryt.fragment.BaseFragment;
-import com.yxh.ryt.fragment.ChuangZuoItemFragment;
-import com.yxh.ryt.fragment.PaiMaiItemFragment;
-import com.yxh.ryt.fragment.RongZiItemFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,8 +33,8 @@ public class AttentionActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attention);
         ButterKnife.bind(this);/*启用注解绑定*/
-        indexChildFragments.add(new AttentionItemFragment());
-        indexChildFragments.add(new AttentionItemFragment());
+        indexChildFragments.add(new AttentionArtItemFragment());
+        indexChildFragments.add(new AttentionUserItemFragment());
         indexChildAdapter = new AttentionIndicatorAdapter(getSupportFragmentManager(),indexChildFragments);
         pager.setOffscreenPageLimit(2);
         pager.setAdapter(indexChildAdapter);
