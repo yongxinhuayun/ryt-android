a350210b0fe01dc448864118ed69be54549cc5d7
iamdurui <durui911225_>
2016-05-19 19:12:25 +0800

    Default

--- Ryt/app/src/main/java/com/yxh/ryt/util/Utils.java
+++ Ryt/app/src/main/java/com/yxh/ryt/util/Utils.java
@@ -254,6 +254,37 @@ public class Utils {
 		/*bitmap.recycle();*/
 		return resizedBitmap;
 	}
+	public static int readPictureDegree1(String path) {
+		int degree  = 0;
+		try {
+			ExifInterface exifInterface = new ExifInterface(path);
+			int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+			switch (orientation) {
+				case ExifInterface.ORIENTATION_ROTATE_90:
+					degree = 90;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_180:
+					degree = 180;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_270:
+					degree = 270;
+					break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return degree;
+	}
+	public static Bitmap rotaingImageView(int angle , Bitmap bitmap) {
+		//旋转图片 动作
+		Matrix matrix = new Matrix();;
+		matrix.postRotate(angle);
+		System.out.println("angle2=" + angle);
+		// 创建新的图片
+		Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+				bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+		return resizedBitmap;
+	}
 	@TargetApi(Build.VERSION_CODES.KITKAT)
 	public static String getFilePathFromUri( Uri uri,Context mContext) {
 		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
