b45468fdd54872687ce19f183f01d4848716af9f
iamdurui <durui911225_>
2016-05-26 12:38:16 +0800

    Default

--- Ryt/app/src/main/java/com/yxh/ryt/validations/PasswordValidation.java
+++ Ryt/app/src/main/java/com/yxh/ryt/validations/PasswordValidation.java
@@ -40,7 +40,7 @@ public class PasswordValidation extends ValidationExecutor {
 			ToastUtil.showShort(AppApplication.getSingleContext(), "密码不能为空！");
 			return false;
 		}
-		String regex = "^[a-zA-Z]|[0-9]{6,16}$";
+		String regex = "^[a-zA-Z0-9]{6,10}$";
 		boolean result = Pattern.compile(regex).matcher(text).find();
 		if (!result) {
 			ToastUtil.showShort(AppApplication.getSingleContext(), "密码格式不正确！");
