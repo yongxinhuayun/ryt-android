b2bef1f6682336d841b9e30ee00eface6ffe6994
iamdurui <durui911225_>
2016-05-27 16:39:22 +0800

    Default

--- Ryt/app/src/main/java/com/yxh/ryt/receiver/WxLoginBroadcastReciver.java
+++ Ryt/app/src/main/java/com/yxh/ryt/receiver/WxLoginBroadcastReciver.java
@@ -10,14 +10,22 @@ import com.yxh.ryt.Constants;
  * Created by 吴洪杰 on 2016/4/6.
  */
 public class WxLoginBroadcastReciver extends BroadcastReceiver {
+    private WxLoginCallBack callBack;
+
+    public WxLoginBroadcastReciver(WxLoginCallBack callBack) {
+        this.callBack = callBack;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (Constants.WX_LOGIN_ACTION.equals(action)) {
             String wxuserStr = intent.getExtras().getString("wxuser");
             System.out.println("2222222222222222222222222222222"+wxuserStr);
-//                AppApplication.gwxuser = AppApplication.getSingleGson().fromJson(wxuserStr, Wxuser.class);
+            callBack.response(wxuserStr);
         }
     }
-
+    public interface WxLoginCallBack{
+        public void response(String wxUser);
+    }
 }
