eb3c203c74cba87f78bb9fe0ec6649496127aa0e
iamdurui <durui911225_>
2016-06-01 15:43:14 +0800

    Default

--- Ryt/app/src/main/java/com/yxh/ryt/receiver/MyReceiver.java
+++ Ryt/app/src/main/java/com/yxh/ryt/receiver/MyReceiver.java
@@ -66,11 +66,11 @@ public class MyReceiver extends BroadcastReceiver {
 		} else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
         	//打开自定义的Activity
-        	Intent i = new Intent(context, ShowMsgActivity.class);
+        	/*Intent i = new Intent(context, ShowMsgActivity.class);
         	i.putExtras(bundle);
         	//i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         	i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP );
-        	context.startActivity(i);
+        	context.startActivity(i);*/
         	
         } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
