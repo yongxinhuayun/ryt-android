2aea95d23487d4fafa674a0f8cd077a5b8a2b1aa
iamdurui <durui911225_>
2016-06-12 15:49:25 +0800

    Default

--- Ryt/app/src/main/assets/shop2016/js/local/creation/app.js
+++ Ryt/app/src/main/assets/shop2016/js/local/creation/app.js
@@ -1,12 +1,16 @@
 /**
- * Created by Administrator on 2016/6/7 0007.
- */
+* Created by Administrator on 2016/6/7 0007.
+*/
 
 
 function redirectUser(userid) {
-    //console.log("test");
-    window.demo.clickOnAndroid(userid);
-// window.location.href = "rongyitou://userid:skldjflksdjflk"
+    console.log("test");
+//        window.location.href = "rongyitou://userid:skldjflksdjflk"
+        window.demo.clickOnAndroid(userid);
 }
 
-hostName = "http://192.168.1.43";                           //服务器域名
+function redirectComment(artworkId, currentUserId, messageId, fatherCommentId) {
+    window.location.href = "";
+}       //跳转到评论
+
+hostName = "http://192.168.1.75:8080";                           //服务器域名
