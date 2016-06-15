9637d192cc38de022e3b11e16ed27ea6df0301b4
yangzhenjie0 <jie0122>
2016-06-15 18:29:32 +0800

    aa

--- Ryt/app/src/main/java/com/yxh/ryt/activity/EditBriefActivity.java
+++ Ryt/app/src/main/java/com/yxh/ryt/activity/EditBriefActivity.java
@@ -1,7 +1,5 @@
 package com.yxh.ryt.activity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +9,7 @@ import com.yxh.ryt.AppApplication;
 import com.yxh.ryt.Constants;
 import com.yxh.ryt.R;
 import com.yxh.ryt.callback.RegisterCallBack;
+import com.yxh.ryt.fragment.UserJianJieFragment;
 import com.yxh.ryt.util.EncryptUtil;
 import com.yxh.ryt.util.NetRequestUtil;
 import com.yxh.ryt.util.ToastUtil;
@@ -20,12 +19,12 @@ import java.util.Map;
 
 import okhttp3.Call;
 
-public class EditBriefActivity extends Activity implements View.OnClickListener {
+public class EditBriefActivity extends BaseActivity implements View.OnClickListener {
 
     private TextView back;
     private TextView save;
     private EditText et_brief;
-    private int resultCode = 102;
+    private UserJianJieFragment mUserJianJieFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +33,41 @@ public class EditBriefActivity extends Activity implements View.OnClickListener 
         back = (TextView) findViewById(R.id.tv_back);
         save = (TextView) findViewById(R.id.tv_save);
         et_brief = (EditText) findViewById(R.id.et_brief);
+        mUserJianJieFragment = new UserJianJieFragment();
         back.setOnClickListener(this);
         save.setOnClickListener(this);
     }
+/*
+    public void setMainListener(OnMainListener mainListener) {
+        this.mainListener = mainListener;
+    }
+
+    private OnMainListener mainListener;
+
+// 绑定接口
+
+    @Override
+
+    public void onAttachFragment(Fragment fragment) {
+
+        try {
+
+            mainListener = (OnMainListener)mUserJianJieFragment;
+
+        } catch (Exception e) {
+
+            throw new ClassCastException(this.toString() + " must implementOnMainListener");
+
+        }
+
+        super.onAttachFragment(fragment);
+
+    }
+    // 接口
+
+    public interface OnMainListener {
+         void onMainAction();
+    }*/
 
     @Override
     public void onClick(View v) {
@@ -64,10 +95,9 @@ public class EditBriefActivity extends Activity implements View.OnClickListener 
                     @Override
                     public void onResponse(Map<String, Object> response) {
                         ToastUtil.show(getApplicationContext(),"保存成功",0);
-                        Intent mIntent = new Intent();
-                        mIntent.putExtra("content", et_brief.getText().toString());
-                        // 设置结果，并进行传送
-                        setResult(resultCode, mIntent);
+                       /* if(mUserJianJieFragment instanceof OnMainListener){
+                            mUserJianJieFragment.onMainAction();
+                        }*/
                         finish();
                     }
                 });
