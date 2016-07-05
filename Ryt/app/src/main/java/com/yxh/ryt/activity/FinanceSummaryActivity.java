0d92547c5add9b0b55baf45d8fe00ab593c99877
iamdurui <durui911225_>
2016-07-05 10:32:36 +0800

    default

---
+++ Ryt/app/src/main/java/com/yxh/ryt/activity/FinanceSummaryActivity.java
@@ -0,0 +1,17 @@
+package com.yxh.ryt.activity;
+
+import android.os.Bundle;
+
+import com.yxh.ryt.R;
+
+/**
+ * Created by Administrator on 2016/7/5.
+ */
+public class FinanceSummaryActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_financesummary);
+
+    }
+}
